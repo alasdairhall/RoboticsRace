d96ab9f988cce0d46eb0712208e9524ef85e498d
chilichilimo <afshar.m.95@gmail.com>
2016-05-17 12:35:29 +0100

    getIr

--- sensing.c
+++ sensing.c
@@ -31,6 +31,20 @@ void moveSquare(int direction)
   drive_goto(123,123); //40CM
 }
 
+int getIR() {
+    irLeft = 0;
+    irRight = 0;
+    for (int dacVal = 0; dacVal < 160; dacVal += 8) {
+        dac_ctr(26, 0, dacVal);
+        freqout(11, 1, 38000);
+        irLeft += input(10);
+
+        dac_ctr(27, 1, dacVal);
+        freqout(1, 1, 38000);
+        irRight += input(2);
+    }
+}
+
 int frontChecker()
 {
   int result;
